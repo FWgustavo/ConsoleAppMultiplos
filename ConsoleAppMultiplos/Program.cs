b9dbd5f22b402407ec730c2b8fcc17a36a0517df
Usuario <Usuario@CPD02-M01>
2024-03-26 10:47:18 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleAppMultiplos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppMultiplos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escreva o número que deseja saber: ");
+            int vezes = int.Parse(Console.ReadLine());
+
+            int cont = vezes;
+            while (cont <= 100) 
+            {
+                Console.WriteLine(cont);
+                 
+                cont += vezes;
+            }
+            Console.ReadKey();
+        }
+    }
+}
